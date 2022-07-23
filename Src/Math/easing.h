828f967710bbbaaf130407d0e6845e2ac3e0dead
shirokuma1101 <71858151+shirokuma1101@users.noreply.github.com>
2022-07-24 00:46:47 +0900

    update: 名前空間内のみで使用する関数をprivate化

--- Src/Math/easing.h
+++ Src/Math/easing.h
@@ -2,12 +2,16 @@
 
 #include <cmath>
 
+#include "..\Utility\macro.h"
+
 #include "constant.h"
 
 namespace easing {
 
     using constant::fPI;
 
+    NAMESPACE_EXTERNAL
+    NAMESPACE_INTERNAL_BEGIN
     inline float In(float n, float pow) noexcept {
         return std::pow(n, pow);
     }
@@ -20,11 +24,7 @@ namespace easing {
             ? std::pow(2.f, pow - 1.f) * std::pow(n, pow)
             : 1.f - std::pow(-2.f * n + 2.f, pow) / 2.f;
     }
-
-    inline float ConstantVelocity(float n) noexcept {
-        //return utility::math::Normalize(n, 0.f, 1.f);
-    }
-
+    NAMESPACE_INTERNAL_END
     
     // Sine
 
@@ -41,49 +41,49 @@ namespace easing {
     // Quad
 
     inline float InQuad(float n) noexcept {
-        return In(n, 2.f);
+        return detail::In(n, 2.f);
     }
     inline float OutQuad(float n) noexcept {
-        return Out(n, 2.f);
+        return detail::Out(n, 2.f);
     }
     inline float InOutQuad(float n) noexcept {
-        return InOut(n, 5.f);
+        return detail::InOut(n, 5.f);
     }
 
     // Cubic
 
     inline float InCubic(float n) noexcept {
-        return In(n, 3.f);
+        return detail::In(n, 3.f);
     }
     inline float OutCubic(float n) noexcept {
-        return Out(n, 3.f);
+        return detail::Out(n, 3.f);
     }
     inline float InOutCubic(float n) noexcept {
-        return InOut(n, 3.f);
+        return detail::InOut(n, 3.f);
     }
 
     // Quart
 
     inline float InQuart(float n) noexcept {
-        return In(n, 4.f);
+        return detail::In(n, 4.f);
     }
     inline float OutQuart(float n) noexcept {
-        return Out(n, 4.f);
+        return detail::Out(n, 4.f);
     }
     inline float InOutQuart(float n) noexcept {
-        return InOut(n, 4.f);
+        return detail::InOut(n, 4.f);
     }
 
     // Quint
 
     inline float InQuint(float n) noexcept {
-        return In(n, 5.f);
+        return detail::In(n, 5.f);
     }
     inline float OutQuint(float n) noexcept {
-        return Out(n, 5.f);
+        return detail::Out(n, 5.f);
     }
     inline float InOutQuint(float n) noexcept {
-        return InOut(n, 5.f);
+        return detail::InOut(n, 5.f);
     }
 
 }
