79194bdce4d2f1367aefdf78620a564d7687419d
shirokuma1101 <71858151+shirokuma1101@users.noreply.github.com>
2022-07-24 00:47:04 +0900

    add: ソースを追加

---
+++ Src/Utility/macro.h
@@ -0,0 +1,8 @@
+#pragma once
+
+
+// namespace private
+
+#define NAMESPACE_EXTERNAL       namespace detail {} namespace {
+#define NAMESPACE_INTERNAL_BEGIN namespace detail {
+#define NAMESPACE_INTERNAL_END   }
