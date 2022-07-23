f35267bbd020d06dc55d2e7a9d428564bad96d3d
shirokuma1101 <71858151+shirokuma1101@users.noreply.github.com>
2022-07-24 04:12:39 +0900

    fix: tabをspaceに変更

--- Src/Math/easing.h
+++ Src/Math/easing.h
@@ -3,6 +3,9 @@
 #include <cmath>
 
 #include "..\Utility\macro.h"
+#define NAMESPACE_EXTERNAL       namespace detail {} namespace {
+#define NAMESPACE_INTERNAL_BEGIN namespace detail {
+#define NAMESPACE_INTERNAL_END   }
 
 #include "constant.h"
 
--- Src/Thread/SimpleThreadManager/SimpleThreadManager.h
+++ Src/Thread/SimpleThreadManager/SimpleThreadManager.h
@@ -7,56 +7,56 @@
 class SimpleThreadManager {
 public:
 
-	using ID = std::thread::id;
+    using ID = std::thread::id;
 
-	SimpleThreadManager() noexcept {
-		m_upThreads.clear();
-	}
-	~SimpleThreadManager() noexcept {
-		for (auto&& e : m_upThreads) {
-			e.second->SyncEnd();
-		}
-		m_upThreads.clear();
-	}
+    SimpleThreadManager() noexcept {
+        m_upThreads.clear();
+    }
+    ~SimpleThreadManager() noexcept {
+        for (auto&& e : m_upThreads) {
+            e.second->SyncEnd();
+        }
+        m_upThreads.clear();
+    }
 
-	template<class Func, class Inst, class...Args>
-	ID Create(Func func, Inst inst, Args... args) {
-		std::unique_ptr<SimpleUniqueThread> upThr = std::make_unique<SimpleUniqueThread>();
-		upThr->Create(func, inst, args...);
-		return m_upThreads.emplace(upThr->GetID(), std::move(upThr)).first;
-	}
+    template<class Func, class Inst, class...Args>
+    ID Create(Func func, Inst inst, Args... args) {
+        std::unique_ptr<SimpleUniqueThread> upThr = std::make_unique<SimpleUniqueThread>();
+        upThr->Create(func, inst, args...);
+        return m_upThreads.emplace(upThr->GetID(), std::move(upThr)).first;
+    }
 
-	//template<class Func, class Inst, class T*, class...Args>
-	//ID Create(Func func, Inst inst, T* ret, Args... args) {
-	//	std::unique_ptr<SimpleUniqueThread> upThr = std::make_unique<SimpleUniqueThread>();
-	//	upThr->Create(func, inst, ret, args...);
-	//	return m_upThreads.emplace(upThr->GetID(), std::move(upThr)).first;
-	//}
+    //template<class Func, class Inst, class T*, class...Args>
+    //ID Create(Func func, Inst inst, T* ret, Args... args) {
+    //	std::unique_ptr<SimpleUniqueThread> upThr = std::make_unique<SimpleUniqueThread>();
+    //	upThr->Create(func, inst, ret, args...);
+    //	return m_upThreads.emplace(upThr->GetID(), std::move(upThr)).first;
+    //}
 
-	bool IsEnd(ID id) const noexcept {
-		if (auto iter = m_upThreads.find(id); iter != m_upThreads.end()) {
-			return iter->second->IsEnd();
-		}
-		else {
-			assert::RaiseAssert("存在していません。");
-			return false;
-		}
-	}
+    bool IsEnd(ID id) const noexcept {
+        if (auto iter = m_upThreads.find(id); iter != m_upThreads.end()) {
+            return iter->second->IsEnd();
+        }
+        else {
+            assert::RaiseAssert("存在していません。");
+            return false;
+        }
+    }
 
-	void SyncEnd(ID* id, SimpleUniqueThread::SyncType sync_type = SimpleUniqueThread::SyncType::JOIN) {
+    void SyncEnd(ID* id, SimpleUniqueThread::SyncType sync_type = SimpleUniqueThread::SyncType::JOIN) {
 
-		if (auto iter = m_upThreads.find(*id); iter != m_upThreads.end()) {
-			iter->second->SyncEnd(sync_type);
-			m_upThreads.erase(*id);
-			*id = ID();
-		}
-		else {
-			assert::RaiseAssert("存在していません。");
-		}
-	}
+        if (auto iter = m_upThreads.find(*id); iter != m_upThreads.end()) {
+            iter->second->SyncEnd(sync_type);
+            m_upThreads.erase(*id);
+            *id = ID();
+        }
+        else {
+            assert::RaiseAssert("存在していません。");
+        }
+    }
 
 private:
 
-	std::map<std::thread::id, std::unique_ptr<SimpleUniqueThread>> m_upThreads;
+    std::map<std::thread::id, std::unique_ptr<SimpleUniqueThread>> m_upThreads;
 
 };
