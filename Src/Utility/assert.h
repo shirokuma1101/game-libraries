e872a3720301fa94d4b52c877dcf7164a16addb3
shirokuma1101 <71858151+shirokuma1101@users.noreply.github.com>
2022-07-25 02:07:38 +0900

    fix: Release時にコンパイルエラーになるのを修正

--- Src/Utility/assert.h
+++ Src/Utility/assert.h
@@ -5,6 +5,7 @@
 
 namespace assert {
 
+#ifdef _DEBUG
     inline void ShowWarning(std::string_view message) {
         _CrtDbgReport(_CRT_WARN, __FILE__, __LINE__, NULL, "warning: %s\n", message.data());
     }
@@ -12,6 +13,10 @@ namespace assert {
     inline void RaiseAssert(std::string_view message) {
         _CrtDbgReport(_CRT_ERROR, __FILE__, __LINE__, NULL, "error: %s\n", message.data());
     }
+#else
+    inline void ShowWarning(std::string_view message) {}
+    inline void RaiseAssert(std::string_view message) {}
+#endif // _DEBUG
 
     inline void ExceptionThrow(std::string_view message) {
         throw std::exception(message.data());
