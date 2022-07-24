01a6f2f4535db48c488354a3ec38ac1319b31dfa
shirokuma1101 <71858151+shirokuma1101@users.noreply.github.com>
2022-07-25 02:12:39 +0900

    fix: スペルミスを修正、存在しないマクロを使用していたのを修正

--- Src/Math/constant.h
+++ Src/Math/constant.h
@@ -2,12 +2,12 @@
 
 namespace constant {
 
-#define CONSATNT_G  9.80665
-#define CONSATNT_PI 3.141592653589793 // acos(-1)
+#define CONSTANT_G  9.80665
+#define CONSTANT_PI 3.141592653589793 // acos(-1)
 
-    constexpr float fG  = static_cast<float>(MATH_G);
-    constexpr float dG  = static_cast<double>(MATH_G);
-    constexpr float fPI = static_cast<float>(MATH_PI);
-    constexpr float dPI = static_cast<double>(MATH_PI);
+    constexpr float fG  = static_cast<float>(CONSTANT_G);
+    constexpr float dG  = static_cast<double>(CONSTANT_G);
+    constexpr float fPI = static_cast<float>(CONSTANT_PI);
+    constexpr float dPI = static_cast<double>(CONSTANT_PI);
 
 }
