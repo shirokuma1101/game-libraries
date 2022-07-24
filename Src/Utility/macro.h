031a37c8c6dfbbd26a0e95d07c713ed878ae5bf7
shirokuma1101 <71858151+shirokuma1101@users.noreply.github.com>
2022-07-25 02:08:06 +0900

    add: singletonマクロを追加

--- Src/Utility/macro.h
+++ Src/Utility/macro.h
@@ -1,8 +1,14 @@
 #pragma once
 
-
-// namespace private
-
 #define NAMESPACE_EXTERNAL       namespace detail {} namespace {
 #define NAMESPACE_INTERNAL_BEGIN namespace detail {
 #define NAMESPACE_INTERNAL_END   }
+
+#define SINGLETON(class_name)          \
+private:                               \
+    class_name() {}                    \
+public:                                \
+    static class_name& GetInstance() { \
+        static class_name Instance;    \
+        return Instance;               \
+    }
