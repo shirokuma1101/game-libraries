86db64d3642faccda82b30222a9dc37aac9893c3
shirokuma1101 <71858151+shirokuma1101@users.noreply.github.com>
2022-07-26 04:29:20 +0900

    fix: 型がfloatのままだったのを修正

--- Src/Math/Constant.h
+++ Src/Math/Constant.h
@@ -5,9 +5,9 @@ namespace constant {
 #define CONSTANT_G  9.80665
 #define CONSTANT_PI 3.141592653589793 // acos(-1)
 
-    constexpr float fG  = static_cast<float>(CONSTANT_G);
-    constexpr float dG  = static_cast<double>(CONSTANT_G);
-    constexpr float fPI = static_cast<float>(CONSTANT_PI);
-    constexpr float dPI = static_cast<double>(CONSTANT_PI);
+    constexpr float  fG  = static_cast<float>(CONSTANT_G);
+    constexpr double dG  = static_cast<double>(CONSTANT_G);
+    constexpr float  fPI = static_cast<float>(CONSTANT_PI);
+    constexpr double dPI = static_cast<double>(CONSTANT_PI);
 
 }
