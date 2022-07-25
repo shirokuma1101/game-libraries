ec48b1da86cf6050d46494931b677b0e883d100f
shirokuma1101 <71858151+shirokuma1101@users.noreply.github.com>
2022-07-26 01:06:23 +0900

    add: 時間制御クラス追加

---
+++ Src/Math/Time.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <chrono>
+
+#include "Convert.h"
+#include "Timer.h"
+
+struct Time {
+
+	enum class Precision {
+		NS,
+		US,
+		MS,
+	};
+
+	double deltaTime;
+
+	void CalcDeltaTime(Precision precision = Precision::MS) {
+		timer.End();
+		switch (precision) {
+		case Precision::NS:
+			deltaTime = convert::NSToS<double>(timer.Duration<Timer::NS>());
+			break;
+		case Precision::US:
+			deltaTime = convert::USToS<double>(timer.Duration<Timer::US>());
+			break;
+		case Precision::MS:
+			deltaTime = convert::MSToS<double>(timer.Duration<Timer::MS>());
+			break;
+		}
+		timer.Start();
+	}
+
+private:
+
+	Timer timer;
+
+};
