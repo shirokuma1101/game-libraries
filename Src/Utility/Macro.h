cff54840837c8feaf3c345d5d3109786267fba19
shirokuma1101 <71858151+shirokuma1101@users.noreply.github.com>
2022-07-26 04:44:36 +0900

    fix: 名前空間のマクロを追加

--- Src/Utility/Macro.h
+++ Src/Utility/Macro.h
@@ -1,6 +1,7 @@
 #pragma once
 
-#define NAMESPACE_EXTERNAL       namespace detail {} namespace {
+#define NAMESPACE_EXTERNAL_BEGIN namespace detail {} namespace {
+#define NAMESPACE_EXTERNAL_END   }
 #define NAMESPACE_INTERNAL_BEGIN namespace detail {
 #define NAMESPACE_INTERNAL_END   }
 
