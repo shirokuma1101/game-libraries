80d218af38ac3e8f5ef96eb1ca5f3145e5c90f56
shirokuma1101 <71858151+shirokuma1101@users.noreply.github.com>
2022-07-26 04:41:33 +0900

    fix: コンストラクタを追加

--- Src/Math/Time.h
+++ Src/Math/Time.h
@@ -7,32 +7,39 @@
 
 struct Time {
 
-	enum class Precision {
-		NS,
-		US,
-		MS,
-	};
+    enum class Precision {
+        NS,
+        US,
+        MS,
+    };
 
-	double deltaTime;
+    Time() noexcept
+        : timer(Timer())
+    {
+        timer.Start();
+        timer.End();
+    }
 
-	void CalcDeltaTime(Precision precision = Precision::MS) {
-		timer.End();
-		switch (precision) {
-		case Precision::NS:
-			deltaTime = convert::NSToS<double>(timer.Duration<Timer::NS>());
-			break;
-		case Precision::US:
-			deltaTime = convert::USToS<double>(timer.Duration<Timer::US>());
-			break;
-		case Precision::MS:
-			deltaTime = convert::MSToS<double>(timer.Duration<Timer::MS>());
-			break;
-		}
-		timer.Start();
-	}
+    double deltaTime;
+
+    void CalcDeltaTime(Precision precision = Precision::MS) {
+        timer.End();
+        switch (precision) {
+        case Precision::NS:
+            deltaTime = convert::NSToS<double>(timer.Duration<Timer::NS>());
+            break;
+        case Precision::US:
+            deltaTime = convert::USToS<double>(timer.Duration<Timer::US>());
+            break;
+        case Precision::MS:
+            deltaTime = convert::MSToS<double>(timer.Duration<Timer::MS>());
+            break;
+        }
+        timer.Start();
+    }
 
 private:
 
-	Timer timer;
+    Timer timer;
 
 };
