be9fa376e5a94ccd0cc21130381f26bd45f2c05b
shirokuma1101 <71858151+shirokuma1101@users.noreply.github.com>
2022-07-28 03:17:35 +0900

    add: 文字変換関数を追加

---
+++ Src/ExternalDependencies/Utility/String.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <string>
+#include <string_view>
+#include "include/strconv.h"
+
+namespace string {
+	inline std::u16string StrToUtf16(std::string_view str) {
+		auto wstr = utf8_to_wide(str.data());
+		return std::u16string(wstr.begin(), wstr.end());
+	}
+}
