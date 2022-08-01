7f53ad2a218be44a294607e812b8ecd3a3272776
shirokuma1101 <71858151+shirokuma1101@users.noreply.github.com>
2022-08-01 17:44:31 +0900

    fix: 定義していた関数を削除

--- Src/Thread/SimpleThreadManager/SimpleUniqueThread.h
+++ Src/Thread/SimpleThreadManager/SimpleUniqueThread.h
@@ -3,28 +3,7 @@
 #include <string_view>
 #include <thread>
 
-#ifndef INC_ASSERT
-namespace assert {
-
-#ifdef _DEBUG
-    inline void ShowWarning(std::string_view message) {
-        _CrtDbgReport(_CRT_WARN, __FILE__, __LINE__, NULL, "warning: %s\n", message.data());
-    }
-
-    inline void RaiseAssert(std::string_view message) {
-        _CrtDbgReport(_CRT_ERROR, __FILE__, __LINE__, NULL, "error: %s\n", message.data());
-    }
-#else
-    inline void ShowWarning(std::string_view message) {}
-    inline void RaiseAssert(std::string_view message) {}
-#endif // _DEBUG
-
-    inline void ExceptionThrow(std::string_view message) {
-        throw std::exception(message.data());
-    }
-
-}
-#endif // !INC_ASSERT
+#include <Utility/Assert.h>
 
 class SimpleUniqueThread {
 public:
