e416025fed6bcd0f19303d3f2b85929ae2c93745
shirokuma1101 <71858151+shirokuma1101@users.noreply.github.com>
2022-08-24 17:23:16 +0900

    add: 現在時刻を実数で取得する関数追加

---
+++ Src/ExternalDependencies/Utility/DateTime.h
@@ -0,0 +1,33 @@
+﻿#include <Windows.h>
+
+namespace date_time {
+    union RealTime {
+        ULARGE_INTEGER ui;
+        FILETIME       ft;
+    };
+
+    inline UINT64 GetRealTime() {
+
+        SYSTEMTIME st;
+        SecureZeroMemory(&st, sizeof(st));
+        GetSystemTime(&st);
+
+        RealTime rt;
+        SecureZeroMemory(&rt, sizeof(rt));
+        SystemTimeToFileTime(&st, &rt.ft);
+
+        return rt.ui.QuadPart;
+    }
+
+    inline SYSTEMTIME GetSystemTime(UINT64 real_time) {
+        RealTime rt;
+        SecureZeroMemory(&rt, sizeof(rt));
+        rt.ui.QuadPart = real_time;
+
+        SYSTEMTIME st;
+        SecureZeroMemory(&st, sizeof(st));
+        FileTimeToSystemTime(&rt.ft, &st);
+
+        return st;
+    }
+}
