4e7599cfc41731fb5c3b6c3c30a36551e16c5b7c
shirokuma1101 <71858151+shirokuma1101@users.noreply.github.com>
2022-08-24 17:21:06 +0900

    update: doubleへの変換に対応

--- Src/Math/Convert.h
+++ Src/Math/Convert.h
@@ -1,4 +1,4 @@
-#pragma once
+﻿#pragma once
 
 #include <vector>
 #include <list>
@@ -10,44 +10,40 @@ namespace convert {
     using constant::fPI;
     using constant::dPI;
 
-    template<class T> constexpr T    ToSquare(T base) noexcept { return base * base; }
-    template<class T> constexpr void ToSquare(T* base)         { *base *= *base; }
+    template<class T> constexpr T      ToSquare(T base)      noexcept { return base * base; }
+    template<class T> constexpr void   ToSquare(T* base)              { *base *= *base; }
 
-    template<class T>          constexpr float ToHalf(T num) noexcept { return num * 0.5f; }
-    template<class U, class T> constexpr U     ToHalf(T num) noexcept { return num * 0.5; }
-    template<class T>          constexpr void  ToHalf(T* num)         { *num *= 0.5f; }
-    //template<class T>          constexpr void  ToHalf(T* num)         { *num *= 0.5; }
+    template<class T> constexpr T      ToHalf(T num)         noexcept { return num * 0.5f; }
+    template<>        constexpr double ToHalf(double num)    noexcept { return num * 0.5; }
+    template<class T> constexpr void   ToHalf(T* num)                 { *num *= 0.5f; }
 
-    template<class T>          constexpr float ToQuarter(T num) noexcept { return num * 0.25f; }
-    template<class U, class T> constexpr U     ToQuarter(T num) noexcept { return num * 0.25; }
-    template<class T>          constexpr void  ToQuarter(T* num)         { *num *= 0.25f; }
-    //template<class T>          constexpr void  ToQuarter(T* num)         { *num *= 0.25; }
+    template<class T> constexpr T      ToQuarter(T num)      noexcept { return num * 0.25f; }
+    template<>        constexpr double ToQuarter(double num) noexcept { return num * 0.25; }
+    template<class T> constexpr void   ToQuarter(T* num)              { *num *= 0.25f; }
 
 
-    template<class T>          constexpr float ToRadians(T deg) noexcept { return deg * (fPI / 180.f); }
-    template<class U, class T> constexpr U     ToRadians(T deg) noexcept { return deg * (fPI / 180); }
-    template<class T>          constexpr void  ToRadians(T* deg)         { *deg *= (fPI / 180.f); }
-    //template<class T>          constexpr void  ToRadians(T* deg)         { *deg *= (fPI / 180); }
+    template<class T> constexpr T      ToRadians(T deg)      noexcept { return deg * (fPI / 180.f); }
+    template<class T> constexpr double ToRadians(double deg) noexcept { return deg * (dPI / 180.0); }
+    template<class T> constexpr void   ToRadians(T* deg)              { *deg *= (fPI / 180.f); }
 
-    template<class T>          constexpr float ToDegrees(T rad) noexcept { return rad * (180.f / fPI); }
-    template<class U, class T> constexpr U     ToDegrees(T rad) noexcept { return rad * (180 / fPI); }
-    template<class T>          constexpr void  ToDegrees(T* rad)         { *rad *= (180.f / fPI); }
-    //template<class T>          constexpr void  ToDegrees(T* rad)         { *rad *= (180 / fPI); }
+    template<class T> constexpr T      ToDegrees(T rad)      noexcept { return rad * (180.f / fPI); }
+    template<>        constexpr double ToDegrees(double rad) noexcept { return rad * (180.0 / dPI); }
+    template<class T> constexpr void   ToDegrees(T* rad)              { *rad *= (180.f / fPI); }
 
 
-    template<class T>          constexpr float NSToS(T t) { return t * 0.000000001f; }
-    template<class U, class T> constexpr U     NSToS(T t) { return t * 0.000000001; }
-    template<class T>          constexpr float USToS(T t) { return t * 0.000001f; }
-    template<class U, class T> constexpr U     USToS(T t) { return t * 0.000001; }
-    template<class T>          constexpr float MSToS(T t) { return t * 0.001f; }
-    template<class U, class T> constexpr U     MSToS(T t) { return t * 0.001; }
+    template<class T> constexpr T      NSToS(T t)      noexcept { return t * 0.000000001f; }
+    template<>        constexpr double NSToS(double t) noexcept { return t * 0.000000001; }
+    template<class T> constexpr T      USToS(T t)      noexcept { return t * 0.000001f; }
+    template<>        constexpr double USToS(double t) noexcept { return t * 0.000001; }
+    template<class T> constexpr T      MSToS(T t)      noexcept { return t * 0.001f; }
+    template<>        constexpr double MSToS(double t) noexcept { return t * 0.001; }
 
-    template<class T>          constexpr T SToNS(T t)  { return t * 1000000000; }
-    template<class T>          constexpr T SToUS(T t)  { return t * 1000000; }
-    template<class T>          constexpr T SToMS(T t)  { return t * 1000; }
-    template<class T>          constexpr T MSToNS(T t) { return t * 1000000; }
-    template<class T>          constexpr T MSToUS(T t) { return t * 1000; }
-    template<class T>          constexpr T USToNS(T t) { return t * 1000; }
+    template<class T> constexpr T SToNS(T t)           noexcept { return t * 1000000000; }
+    template<class T> constexpr T SToUS(T t)           noexcept { return t * 1000000; }
+    template<class T> constexpr T SToMS(T t)           noexcept { return t * 1000; }
+    template<class T> constexpr T MSToNS(T t)          noexcept { return t * 1000000; }
+    template<class T> constexpr T MSToUS(T t)          noexcept { return t * 1000; }
+    template<class T> constexpr T USToNS(T t)          noexcept { return t * 1000; }
 
     template<template<class> class Container, class T>
     inline auto ToContainer(T iterable) {
