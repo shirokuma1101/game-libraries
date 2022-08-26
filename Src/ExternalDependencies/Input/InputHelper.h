f70326d4b5c1f20b38cc61c245d03325a3479203
shirokuma1101 <71858151+shirokuma1101@users.noreply.github.com>
2022-08-27 05:09:30 +0900

    add: InputManagerを追加

---
+++ Src/ExternalDependencies/Input/InputHelper.h
@@ -0,0 +1,85 @@
+﻿#pragma once
+
+#include <Windows.h>
+
+namespace input_helper {
+
+    class KeyData
+    {
+    public:
+
+        constexpr KeyData(int key) noexcept
+            : key(key)
+        {}
+
+        int GetKey() const noexcept {
+            return key;
+        }
+
+        bool GetState(bool is_press_and_hold = false) {
+            if (is_press_and_hold) {
+                if (isPressed) {
+                    return false;
+                }
+            }
+            isPressed = GetState(key);
+            return isPressed;
+        }
+
+        static bool GetState(int key) noexcept {
+            return GetAsyncKeyState(key) & 0x8000;
+        }
+
+    private:
+        
+        const int key;
+        bool      isPressed = false;
+        
+    };
+
+    class MouseData
+    {
+    public:
+        
+        MouseData(HWND hwnd = 0) noexcept
+            : hwnd(hwnd)
+            , point()
+            , beforePoint()
+        {}
+
+        POINT GetPoint() const noexcept {
+            return point;
+        }
+
+        POINT GetBeforePoint() const noexcept {
+            return beforePoint;
+        }
+        
+        POINT GetPosition() noexcept {
+            beforePoint = point;
+            point = GetPosition(hwnd);
+            return point;
+        }
+
+        POINT GetDifference() noexcept {
+            return { point.x - beforePoint.x, point.y - beforePoint.y };
+        }
+
+        static POINT GetPosition(HWND hwnd) {
+            POINT point;
+            GetCursorPos(&point);
+            if (hwnd) {
+                ScreenToClient(hwnd, &point);
+            }
+            return point;
+        }
+
+    private:
+        
+        const HWND hwnd;
+        POINT      point;
+        POINT      beforePoint;
+        
+    };
+    
+}
---
+++ Src/ExternalDependencies/Input/InputManager.h
@@ -0,0 +1,102 @@
+﻿#pragma once
+
+#include <list>
+
+#include "InputHelper.h"
+
+class KeyManager
+{
+public:
+
+    void Update() {
+        for (auto iter = m_keys.begin(); iter != m_keys.end();) {
+            if (iter->GetState()) {
+                ++iter;
+            }
+            else {
+                iter = m_keys.erase(iter);
+            }
+        }
+    }
+
+    bool GetState(int key, bool is_press_and_hold) {
+        for (auto iter = m_keys.begin(); iter != m_keys.end();) {
+            if (iter->GetKey() == key) {
+                if (is_press_and_hold) {
+                    return true;
+                }
+                else {
+                    return false;
+                }
+            }
+            else {
+                ++iter;
+            }
+        }
+        if (input_helper::KeyData::GetState(key)) {
+            m_keys.push_back(input_helper::KeyData(key));
+            return true;
+        }
+        return false;
+    }
+
+private:
+    
+    std::list<input_helper::KeyData> m_keys;
+    
+};
+
+class MouseManager
+{
+public:
+
+    MouseManager(HWND hwnd = 0)
+        : m_mouseData(hwnd)
+    {}
+
+    void Update() {
+        m_mouseData.GetPosition();
+    }
+
+    POINT GetPosition() {
+        return m_mouseData.GetPoint();
+    }
+
+    POINT GetDifference() {
+        return m_mouseData.GetDifference();
+    }
+
+private:
+
+    input_helper::MouseData m_mouseData;
+
+};
+
+class InputManager
+{
+public:
+
+    InputManager(HWND hwnd = 0)
+        : m_keyMgr()
+        , m_mouseMgr(hwnd)
+    {}
+
+    void Update() {
+        m_keyMgr.Update();
+        m_mouseMgr.Update();
+    }
+
+    const KeyManager& GetKeyManager() const {
+        return m_keyMgr;
+    }
+
+    const MouseManager& GetMouseManager() const {
+        return m_mouseMgr;
+    }
+
+private:
+
+    KeyManager m_keyMgr;
+    MouseManager m_mouseMgr;
+    
+};
