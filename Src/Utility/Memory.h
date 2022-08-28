19bb34e1700744667310b1e1806e21c92d67ce2d
shirokuma1101 <71858151+shirokuma1101@users.noreply.github.com>
2022-08-29 06:50:33 +0900

    fix: nullptrを正しく設定できていなかった問題を修正

--- Src/Utility/Memory.h
+++ Src/Utility/Memory.h
@@ -3,17 +3,17 @@
 namespace memory {
 
     template<class T>
-    inline auto SafeRelease(T* p)->decltype(p->Release(), void()) {
-        if (!p) return;
-        p->Release();
-        p = nullptr;
+    inline auto SafeRelease(T** p)->decltype((*p)->Release(), void()) {
+        if (!*p) return;
+        (*p)->Release();
+        *p = nullptr;
     }
 
     template<class T>
-    inline auto SafeRelease(T* p)->decltype(p->release(), void()) {
-        if (!p) return;
-        p->release();
-        p = nullptr;
+    inline auto SafeRelease(T** p)->decltype((*p)->release(), void()) {
+        if (!*p) return;
+        (*p)->release();
+        *p = nullptr;
     }
 
     template<class T>
