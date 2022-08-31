f9692efca9b986ce2a0838b251b488c58c5ccbc5
shirokuma1101 <71858151+shirokuma1101@users.noreply.github.com>
2022-09-01 02:30:48 +0900

    fix: 日本語が変換されない不具合を修正

--- Src/ExternalDependencies/Utility/String.h
+++ Src/ExternalDependencies/Utility/String.h
@@ -7,7 +7,8 @@
 
 namespace string {
     inline std::u16string StrToUtf16(std::string_view str) {
-        auto wstr = utf8_to_wide(str.data());
+        //auto wstr = utf8_to_wide(str.data());
+        auto wstr = sjis_to_wide(str.data());
         return std::u16string(wstr.begin(), wstr.end());
     }
 }
