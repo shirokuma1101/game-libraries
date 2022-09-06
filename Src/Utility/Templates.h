0126a3085bcce972a30763eac87b165c1b203574
shirokuma1101 <71858151+shirokuma1101@users.noreply.github.com>
2022-09-07 03:52:48 +0900

    fix: コードを整形、true版templateを追加

--- Src/Utility/Templates.h
+++ Src/Utility/Templates.h
@@ -7,20 +7,21 @@
 
 namespace templates {
 
-#define TEMPLATES_HAS_FUNC(type, func) templates::HasFunc<type>([](auto&& obj)->decltype(obj.func){})
+    template <class T>
+    constexpr bool true_v = false;
+    template <class T>
+    constexpr bool false_v = false;
 
+
+#define TEMPLATES_HAS_FUNC(type, func) templates::HasFunc<type>([](auto&& obj)->decltype(obj.func){})
     template<class T, class Func>
     constexpr auto HasFunc(Func&& f)->decltype(f(std::declval<T>()), true) {
         return true;
     }
-    
     template<class>
     constexpr bool HasFunc(...) {
         return false;
     }
-
-    template <class T>
-    constexpr bool false_v = false;
 
 }
 
