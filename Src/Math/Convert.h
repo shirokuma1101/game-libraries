5c8f57c4305beb49ff75f78210a922516ffb0430
shirokuma1101 <71858151+shirokuma1101@users.noreply.github.com>
2022-09-08 00:15:26 +0900

    update: 型による関数の部分特殊化へ変更

--- Src/Math/Convert.h
+++ Src/Math/Convert.h
@@ -7,43 +7,71 @@
 #include <vector>
 
 #include "Math/Constant.h"
+#include "Utility/Macro.h" 
+                                                                                                                  
+#define CONVERT_SWITCHING_FLOAT_DOUBLE_FUNC(func_name, arg_name, float_expr, double_expr)                         \
+NAMESPACE_EXTERNAL_BEGIN                                                                                          \
+NAMESPACE_INTERNAL_BEGIN                                                                                          \
+template<class ArgTy, class>                                                                                      \
+struct func_name                { static constexpr auto Func(ArgTy arg_name) noexcept { return float_expr; } };   \
+template<class ArgTy>                                                                                             \
+struct func_name<ArgTy, double> { static constexpr auto Func(ArgTy arg_name) noexcept { return double_expr; } };  \
+NAMESPACE_INTERNAL_END                                                                                            \
+template<class ArgTy>                                                                                             \
+constexpr auto func_name(ArgTy arg_name) noexcept { return detail::func_name<ArgTy, ArgTy>::Func(arg_name); }     \
+template<class ExpectTy, class ArgTy>                                                                             \
+constexpr auto func_name(ArgTy arg_name) noexcept { return detail::func_name<ArgTy, ExpectTy>::Func(arg_name); }  \
+NAMESPACE_EXTERNAL_END
 
 namespace convert {
 
-    template<class T> constexpr T      ToSquare(T base)      noexcept { return base * base; }
-    template<class T> constexpr void   ToSquare(T* base)              { *base *= *base; }
+    template<class T> constexpr T      ToSquare(const T& base) noexcept { return base * base; }
+    template<class T> constexpr void   ToSquare(T* base)                { *base *= *base; }
+    /*
+    NAMESPACE_EXTERNAL_BEGIN
+    NAMESPACE_INTERNAL_BEGIN
+    template<class ArgTy, class> struct ToHalf                   { static constexpr auto Func(ArgTy num) noexcept { return num * 0.5f; } };
+    template<class ArgTy>        struct ToHalf<ArgTy, double>    { static constexpr auto Func(ArgTy num) noexcept { return num * 0.5; } };
+    template<class ArgTy, class> struct ToQuarter                { static constexpr auto Func(ArgTy num) noexcept { return num * 0.25f; } };
+    template<class ArgTy>        struct ToQuarter<ArgTy, double> { static constexpr auto Func(ArgTy num) noexcept { return num * 0.25; } };
+    template<class ArgTy, class> struct ToRadians                { static constexpr auto Func(ArgTy deg) noexcept { return deg * (constant::fPI / 180.f); } };
+    template<class ArgTy>        struct ToRadians<ArgTy, double> { static constexpr auto Func(ArgTy deg) noexcept { return deg * (constant::dPI / 180.0); } };
+    template<class ArgTy, class> struct ToDegrees                { static constexpr auto Func(ArgTy rad) noexcept { return rad * (180.f / constant::fPI); } };
+    template<class ArgTy>        struct ToDegrees<ArgTy, double> { static constexpr auto Func(ArgTy rad) noexcept { return rad * (180.0 / constant::dPI); } };
+    NAMESPACE_INTERNAL_END
+    template<class ArgTy>                 constexpr auto ToHalf(ArgTy num)    noexcept { return detail::ToHalf<ArgTy, ArgTy>::Func(num); }
+    template<class ExpectTy, class ArgTy> constexpr auto ToHalf(ArgTy num)    noexcept { return detail::ToHalf<ArgTy, ExpectTy>::Func(num); }
+    template<class ArgTy>                 constexpr auto ToQuarter(ArgTy num) noexcept { return detail::ToQuarter<ArgTy, ArgTy>::Func(num); }
+    template<class ExpectTy, class ArgTy> constexpr auto ToQuarter(ArgTy num) noexcept { return detail::ToQuarter<ArgTy, ExpectTy>::Func(num); }
+    template<class ArgTy>                 constexpr auto ToRadians(ArgTy deg) noexcept { return detail::ToRadians<ArgTy, ArgTy>::Func(deg); }
+    template<class ExpectTy, class ArgTy> constexpr auto ToRadians(ArgTy deg) noexcept { return detail::ToRadians<ArgTy, ExpectTy>::Func(deg); }
+    template<class ArgTy>                 constexpr auto ToDegrees(ArgTy rad) noexcept { return detail::ToDegrees<ArgTy, ArgTy>::Func(rad); }
+    template<class ExpectTy, class ArgTy> constexpr auto ToDegrees(ArgTy rad) noexcept { return detail::ToDegrees<ArgTy, ExpectTy>::Func(rad); }
+    NAMESPACE_EXTERNAL_END
+    */
+    CONVERT_SWITCHING_FLOAT_DOUBLE_FUNC(ToHalf,    num, num * 0.5f,                    num * 0.5                    );
+    CONVERT_SWITCHING_FLOAT_DOUBLE_FUNC(ToQuarter, num, num * 0.25f,                   num * 0.25                   );
+    CONVERT_SWITCHING_FLOAT_DOUBLE_FUNC(ToRadians, deg, deg * (constant::fPI / 180.f), deg * (constant::dPI / 180.0));
+    CONVERT_SWITCHING_FLOAT_DOUBLE_FUNC(ToDegrees, rad, rad * (180.f / constant::fPI), rad * (180.0 / constant::dPI));
 
-    template<class T> constexpr T      ToHalf(T num)         noexcept { return num * 0.5f; }
-    template<>        constexpr double ToHalf(double num)    noexcept { return num * 0.5; }
-    template<class T> constexpr void   ToHalf(T* num)                 { *num *= 0.5f; }
+    
+    // 1S = 1000MS = 1000000US = 1000000000NS
 
-    template<class T> constexpr T      ToQuarter(T num)      noexcept { return num * 0.25f; }
-    template<>        constexpr double ToQuarter(double num) noexcept { return num * 0.25; }
-    template<class T> constexpr void   ToQuarter(T* num)              { *num *= 0.25f; }
-
-
-    template<class T> constexpr T      ToRadians(T deg)      noexcept { return deg * (constant::fPI / 180.f); }
-    template<class T> constexpr double ToRadians(double deg) noexcept { return deg * (constant::dPI / 180.0); }
-    template<class T> constexpr void   ToRadians(T* deg)              { *deg *= (constant::fPI / 180.f); }
-
-    template<class T> constexpr T      ToDegrees(T rad)      noexcept { return rad * (180.f / constant::fPI); }
-    template<>        constexpr double ToDegrees(double rad) noexcept { return rad * (180.0 / constant::dPI); }
-    template<class T> constexpr void   ToDegrees(T* rad)              { *rad *= (180.f / constant::fPI); }
-
-
-    template<class T> constexpr T      NSToS(T t)      noexcept { return t * 0.000000001f; }
-    template<>        constexpr double NSToS(double t) noexcept { return t * 0.000000001; }
-    template<class T> constexpr T      USToS(T t)      noexcept { return t * 0.000001f; }
-    template<>        constexpr double USToS(double t) noexcept { return t * 0.000001; }
-    template<class T> constexpr T      MSToS(T t)      noexcept { return t * 0.001f; }
-    template<>        constexpr double MSToS(double t) noexcept { return t * 0.001; }
-
-    template<class T> constexpr T SToNS(T t)           noexcept { return t * 1000000000; }
-    template<class T> constexpr T SToUS(T t)           noexcept { return t * 1000000; }
-    template<class T> constexpr T SToMS(T t)           noexcept { return t * 1000; }
-    template<class T> constexpr T MSToNS(T t)          noexcept { return t * 1000000; }
-    template<class T> constexpr T MSToUS(T t)          noexcept { return t * 1000; }
-    template<class T> constexpr T USToNS(T t)          noexcept { return t * 1000; }
+    template<class T> constexpr auto    SToNS(T t)  noexcept { return t * 1000000000; }
+    template<class T> constexpr auto    SToUS(T t)  noexcept { return t * 1000000;    }
+    template<class T> constexpr auto    SToMS(T t)  noexcept { return t * 1000;       }
+    
+    template<class T> constexpr auto    MSToNS(T t) noexcept { return t * 1000000;  }
+    template<class T> constexpr auto    MSToUS(T t) noexcept { return t * 1000;     }
+    CONVERT_SWITCHING_FLOAT_DOUBLE_FUNC(MSToS,  t, t * 0.001f,       t * 0.001      );
+    
+    template<class T> constexpr auto    USToNS(T t) noexcept { return t * 1000;     }
+    CONVERT_SWITCHING_FLOAT_DOUBLE_FUNC(USToMS, t, t * 0.001f,       t * 0.001      );
+    CONVERT_SWITCHING_FLOAT_DOUBLE_FUNC(USToS,  t, t * 0.000001f,    t * 0.000001   );
+    
+    CONVERT_SWITCHING_FLOAT_DOUBLE_FUNC(NSToUS, t, t * 0.001f,       t * 0.001      );
+    CONVERT_SWITCHING_FLOAT_DOUBLE_FUNC(NSToMS, t, t * 0.000001f,    t * 0.000001   );
+    CONVERT_SWITCHING_FLOAT_DOUBLE_FUNC(NSToS,  t, t * 0.000000001f, t * 0.000000001);
 
     template<template<class> class Container, class T>
     inline auto ToContainer(T iterable) {
@@ -54,12 +82,10 @@ namespace convert {
         }
         return container;
     }
-
     template<class T, class U>
     inline std::vector<T> ToVector(U iterable) {
         return ToContainer<std::vector>(iterable);
     }
-
     template<class T, class U>
     inline std::list<T> ToList(U iterable) {
         return ToContainer<std::list>(iterable);
@@ -83,7 +109,6 @@ namespace convert {
     }
 
     inline float Normalize(float x, float x_min, float x_max, float y_start = 0.f, float y_end = 1.f) {
-        
         if (x_min != 0.f) {
             x -= x_min;
             x_max -= x_min;
