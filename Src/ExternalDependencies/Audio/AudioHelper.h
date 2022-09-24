308a7e021280aa102805011a532a16fa59773007
shirokuma1101 <71858151+shirokuma1101@users.noreply.github.com>
2022-09-25 00:24:40 +0900

    add: AudioManagerを追加

---
+++ Src/ExternalDependencies/Audio/AudioHelper.h
@@ -0,0 +1,170 @@
+﻿#pragma once
+
+#ifndef GAME_LIBRARIES_EXTERNALDEPENDENCIES_AUDIO_AUDIOHELPER_H_
+#define GAME_LIBRARIES_EXTERNALDEPENDENCIES_AUDIO_AUDIOHELPER_H_
+
+#include <memory>
+#include <string>
+#include <string_view>
+
+#include "Utility/Assert.h"
+
+#include "Audio.h"
+#include "SimpleMath.h"
+#include "strconv.h"
+
+#ifndef AUDIOHELPER_DISABLE_LINK_LIBS
+#pragma comment(lib, "DirectXTK.lib")
+#pragma comment(lib, "DirectXTKAudioWin8.lib")
+#endif
+
+namespace audio_helper {
+    
+    class SoundData
+    {
+    public:
+
+        SoundData(std::string_view file_path) noexcept
+            : m_filePath(file_path)
+        {}
+        SoundData(DirectX::AudioEngine* engine, std::string_view file_path)
+            : m_filePath(file_path)
+        {
+            Load(engine);
+        }
+
+        bool Load(DirectX::AudioEngine* engine) {
+            try {
+                m_upSoundEffect = std::make_unique<DirectX::SoundEffect>(engine, sjis_to_wide(m_filePath.data()).c_str());
+                return true;
+            }
+            catch (...) {
+                assert::RaiseAssert("Failed to load sound effect.");
+                return false;
+            }
+        }
+
+        std::unique_ptr<DirectX::SoundEffectInstance> CreateInstance(DirectX::SOUND_EFFECT_INSTANCE_FLAGS flags) {
+            if (!m_upSoundEffect) {
+                return nullptr;
+            }
+
+            return m_upSoundEffect->CreateInstance(flags);
+        }
+
+    private:
+
+        const std::string                     m_filePath;
+        std::unique_ptr<DirectX::SoundEffect> m_upSoundEffect = nullptr;
+        
+    };
+
+    class SoundInstance
+    {
+    public:
+
+        SoundInstance(const std::shared_ptr<SoundData> sound_data) noexcept
+            : m_spSoundData(sound_data)
+        {}
+        SoundInstance(DirectX::SOUND_EFFECT_INSTANCE_FLAGS flags, const std::shared_ptr<SoundData> sound_data)
+            : m_spSoundData(sound_data)
+        {
+            CreateInstance(flags);
+        }
+
+        bool CreateInstance(DirectX::SOUND_EFFECT_INSTANCE_FLAGS flags) {
+            if (!m_spSoundData) {
+                return false;
+            }
+            m_upSoundEffectInstance = m_spSoundData->CreateInstance(flags);
+            if (flags & DirectX::SOUND_EFFECT_INSTANCE_FLAGS::SoundEffectInstance_Use3D) {
+                m_upAudioEmitter = std::make_unique<DirectX::AudioEmitter>();
+            }
+            return static_cast<bool>(m_upSoundEffectInstance);
+        }
+
+        const std::shared_ptr<SoundData>& GetSoundData() const noexcept {
+            return m_spSoundData;
+        }
+
+        const std::unique_ptr<DirectX::SoundEffectInstance>& GetSoundEffectInstance() const noexcept {
+            return m_upSoundEffectInstance;
+        }
+
+        /**************************************************
+        * DirectX::SoundEffectInstance function wrap
+        **************************************************/
+        
+        void Play(bool loop = false) noexcept {
+            if (!m_upSoundEffectInstance) return;
+            m_upSoundEffectInstance->Play(loop);
+        }
+        void Stop(bool immediate = true) noexcept {
+            if (!m_upSoundEffectInstance) return;
+            m_upSoundEffectInstance->Stop(immediate);
+        }
+        void Pause() noexcept {
+            if (!m_upSoundEffectInstance) return;
+            m_upSoundEffectInstance->Pause();
+        }
+        void Resume() noexcept {
+            if (!m_upSoundEffectInstance) return;
+            m_upSoundEffectInstance->Resume();
+        }
+
+        void SetVolume(float volume) noexcept {
+            if (!m_upSoundEffectInstance) return;
+            m_upSoundEffectInstance->SetVolume(volume);
+        }
+        void SetPitch(float pitch) noexcept {
+            if (!m_upSoundEffectInstance) return;
+            m_upSoundEffectInstance->SetPitch(pitch);
+        }
+        void SetPan(float pan) noexcept {
+            if (!m_upSoundEffectInstance) return;
+            m_upSoundEffectInstance->SetPan(pan);
+        }
+
+        void Apply3D(const DirectX::AudioListener& listener, const DirectX::SimpleMath::Vector3& position, bool rhcoords = false) {
+            if (!m_upSoundEffectInstance) return;
+            m_upAudioEmitter->SetPosition(position);
+            m_upSoundEffectInstance->Apply3D(listener, *m_upAudioEmitter, rhcoords);
+        }
+        
+        bool IsLooped() const noexcept {
+            if (!m_upSoundEffectInstance) return false;
+            return m_upSoundEffectInstance->IsLooped();
+        }
+        
+        bool IsState(DirectX::SoundState state) const noexcept {
+            if (!m_upSoundEffectInstance) {
+                return false;
+            }
+            return (m_upSoundEffectInstance->GetState() == state);
+        }
+
+        unsigned int GetChannelCount() const noexcept {
+            if (!m_upSoundEffectInstance) {
+                return 0;
+            }
+            return m_upSoundEffectInstance->GetChannelCount();
+        }
+
+        DirectX::IVoiceNotify* GetVoiceNotify() const noexcept {
+            if (!m_upSoundEffectInstance) {
+                return nullptr;
+            }
+            return m_upSoundEffectInstance->GetVoiceNotify();
+        }
+
+    private:
+
+        const std::shared_ptr<SoundData>              m_spSoundData           = nullptr;
+        std::unique_ptr<DirectX::SoundEffectInstance> m_upSoundEffectInstance = nullptr;
+        std::unique_ptr<DirectX::AudioEmitter>        m_upAudioEmitter        = nullptr;
+        
+    };
+    
+}
+
+#endif
---
+++ Src/ExternalDependencies/Audio/AudioManager.h
@@ -0,0 +1,136 @@
+﻿#pragma once
+
+#ifndef GAME_LIBRARIES_EXTERNALDEPENDENCIES_AUDIO_AUDIOMANAGER_H_
+#define GAME_LIBRARIES_EXTERNALDEPENDENCIES_AUDIO_AUDIOMANAGER_H_
+
+#include <memory>
+#include <string>
+#include <string_view>
+#include <unordered_map>
+#include <Windows.h>
+
+#include "Utility/Assert.h"
+#include "ExternalDependencies/Audio/AudioHelper.h"
+
+class AudioManager
+{
+public:
+
+    enum class PlayFlags {
+        None   = 1 << 0,
+        Loop   = 1 << 1,
+        Unique = 1 << 2,
+        Play3D = 1 << 3,
+    };
+    //friend bool operator|(PlayFlags lhs, PlayFlags rhs);
+    //friend bool operator&(PlayFlags lhs, PlayFlags rhs);
+    friend bool operator|(PlayFlags lhs, PlayFlags rhs) {
+        using UnderlyingTypeT = std::underlying_type_t<AudioManager::PlayFlags>;
+        return (static_cast<UnderlyingTypeT>(lhs) | static_cast<UnderlyingTypeT>(rhs));
+    }
+    friend bool operator&(PlayFlags lhs, PlayFlags rhs) {
+        using UnderlyingTypeT = std::underlying_type_t<AudioManager::PlayFlags>;
+        return (static_cast<UnderlyingTypeT>(lhs) & static_cast<UnderlyingTypeT>(rhs));
+    }
+
+    ~AudioManager() {
+        Release();
+    }
+
+    void Init(DirectX::AUDIO_ENGINE_REVERB reverb_flag = DirectX::AUDIO_ENGINE_REVERB::Reverb_Default) {
+        if (FAILED(CoInitializeEx(nullptr, COINIT::COINIT_MULTITHREADED))) {
+            assert::RaiseAssert("Failed to initialize COM.");
+        }
+        
+        DirectX::AUDIO_ENGINE_FLAGS flags = DirectX::AUDIO_ENGINE_FLAGS::AudioEngine_Default;
+#ifdef _DEBUG
+        flags |= DirectX::AUDIO_ENGINE_FLAGS::AudioEngine_Debug;
+#endif
+        m_upAudioEngine = std::make_unique<DirectX::AudioEngine>(flags);
+        m_upAudioEngine->SetReverb(reverb_flag);
+        m_listener.OrientFront = { 0.f, 0.f, 1.f };
+    }
+
+    void Update(const DirectX::SimpleMath::Vector3& position = {}, const DirectX::SimpleMath::Vector3& direction = {}) {
+        if (!m_upAudioEngine->Update()) {
+            if (m_upAudioEngine->IsCriticalError()) {
+                assert::RaiseAssert("Audio engine critical error.");
+            }
+        }
+        m_listener.Position = position;
+        m_listener.OrientFront = direction;
+
+        for (auto iter = m_spSoundInstances.begin(); iter != m_spSoundInstances.end();) {
+            if (iter->second->IsState(DirectX::SoundState::STOPPED)) {
+                iter = m_spSoundInstances.erase(iter);
+            }
+            else {
+                ++iter;
+            }
+        }
+    }
+
+    std::shared_ptr<audio_helper::SoundInstance> GetSoundInstance(std::string_view sound_name) {
+        if (auto iter = m_spSoundInstances.find(sound_name.data()); iter != m_spSoundInstances.end()) {
+            return iter->second;
+        }
+        return nullptr;
+    }
+
+    void SetAudio(std::string_view sound_name, std::string_view file_path) {
+        m_spSoundData.emplace(sound_name, std::make_shared<audio_helper::SoundData>(m_upAudioEngine.get(), file_path));
+    }
+
+    std::shared_ptr<audio_helper::SoundInstance> Play(std::string_view sound_name, PlayFlags play_flags = PlayFlags::None, const DirectX::SimpleMath::Vector3& position = {}) {
+        if (play_flags & PlayFlags::Unique) {
+            if (auto iter = m_spSoundInstances.find(sound_name.data()); iter != m_spSoundInstances.end()) {
+                return iter->second;
+            }
+        }
+        if (auto iter = m_spSoundData.find(sound_name.data()); iter != m_spSoundData.end()) {
+            DirectX::SOUND_EFFECT_INSTANCE_FLAGS flags = DirectX::SoundEffectInstance_Default;
+            if (play_flags & PlayFlags::Play3D) {
+                flags |= DirectX::SoundEffectInstance_Use3D | DirectX::SoundEffectInstance_ReverbUseFilters;
+            }
+            auto sound_instance = std::make_shared<audio_helper::SoundInstance>(flags, iter->second);
+            
+            if (play_flags & PlayFlags::Play3D) {
+                sound_instance->Apply3D(m_listener, position);
+            }
+            sound_instance->Play(play_flags & PlayFlags::Loop);
+
+            m_spSoundInstances.emplace(sound_name, sound_instance);
+            return sound_instance;
+        }
+        return nullptr;
+    }
+
+    void StopAllSound() {
+        for (auto&& e : m_spSoundInstances) {
+            e.second->GetSoundEffectInstance()->Stop();
+        }
+    }
+
+private:
+
+    void Release() {
+        CoUninitialize();
+    }
+
+    std::unique_ptr<DirectX::AudioEngine>                                              m_upAudioEngine = nullptr;
+    DirectX::AudioListener                                                             m_listener;
+    std::unordered_map<std::string, std::shared_ptr<audio_helper::SoundData>>          m_spSoundData;
+    std::unordered_multimap<std::string, std::shared_ptr<audio_helper::SoundInstance>> m_spSoundInstances;
+
+};
+
+//bool operator|(AudioManager::PlayFlags lhs, AudioManager::PlayFlags rhs) {
+//    using UnderlyingTypeT = std::underlying_type_t<AudioManager::PlayFlags>;
+//    return (static_cast<UnderlyingTypeT>(lhs) | static_cast<UnderlyingTypeT>(rhs));
+//}
+//bool operator&(AudioManager::PlayFlags lhs, AudioManager::PlayFlags rhs) {
+//    using UnderlyingTypeT = std::underlying_type_t<AudioManager::PlayFlags>;
+//    return (static_cast<UnderlyingTypeT>(lhs) & static_cast<UnderlyingTypeT>(rhs));
+//}
+
+#endif
