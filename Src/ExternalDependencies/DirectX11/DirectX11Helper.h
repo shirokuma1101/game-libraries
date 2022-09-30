1b5c71438ca04eebea6864e311c6fedecfb81874
shirokuma1101 <71858151+shirokuma1101@users.noreply.github.com>
2022-09-30 19:46:43 +0900

    add: DirectX11Helperを追加

---
+++ Src/ExternalDependencies/DirectX11/DirectX11Helper.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+
+#ifndef GAME_LIBRARIES_EXTERNALDEPENDENCIES_DIRECTX11_DIRECTX11HELPER_H_
+#define GAME_LIBRARIES_EXTERNALDEPENDENCIES_DIRECTX11_DIRECTX11HELPER_H_
+
+#include <d3d11.h>
+
+#include "Utility/Memory.h"
+
+namespace directx11_helper {
+
+    inline D3D11_TEXTURE2D_DESC GetTextureInfo(ID3D11View* view) {
+        D3D11_TEXTURE2D_DESC td{};
+        ID3D11Resource* resource = nullptr;
+        ID3D11Texture2D* texture2d = nullptr;
+        
+        memset(&td, 0, sizeof(td));
+        view->GetResource(&resource);
+        if (SUCCEEDED(resource->QueryInterface<ID3D11Texture2D>(&texture2d))) {
+            texture2d->GetDesc(&td);
+            memory::SafeRelease(&texture2d);
+        }
+        memory::SafeRelease(&resource);
+    }
+
+}
+
+#endif
