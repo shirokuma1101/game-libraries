0243403069c37813c7a3071a3ba45ce4740250e6
shirokuma1101 <71858151+shirokuma1101@users.noreply.github.com>
2022-10-01 01:03:21 +0900

    update: __cplusplus用のマクロを追加

--- Src/Utility/Macro.h
+++ Src/Utility/Macro.h
@@ -3,6 +3,13 @@
 #ifndef GAME_LIBRARIES_UTILITY_MACRO_H_
 #define GAME_LIBRARIES_UTILITY_MACRO_H_
 
+#define CPP98 199711L
+#define CPP11 201103L
+#define CPP14 201402L
+#define CPP17 201703L
+#define CPP20 202002L
+#define CPP23 202399L // 2023XXL
+
 #define NAMESPACE_EXTERNAL_BEGIN namespace detail {} namespace {
 #define NAMESPACE_EXTERNAL_END   }
 #define NAMESPACE_INTERNAL_BEGIN namespace detail {
