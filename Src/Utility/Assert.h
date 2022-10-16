0aac9d1c229adc7402dfd7553e8894308a624d73
shirokuma1101 <71858151+shirokuma1101@users.noreply.github.com>
2022-10-17 00:55:48 +0900

    update: fileとlineを引数で指定できるように変更

--- Src/Utility/Assert.h
+++ Src/Utility/Assert.h
@@ -7,24 +7,26 @@
 #include <string_view>
 
 namespace assert {
+    
+#define ASSERT_FILE_LINE __FILE__, __LINE__
 
 #ifdef _DEBUG
-    inline void ShowWarning(std::string_view message) {
-        _CrtDbgReport(_CRT_WARN, __FILE__, __LINE__, NULL, "warning: %s\n", message.data());
+    inline void ShowWarning(char const* file, int line, std::string_view message) {
+        _CrtDbgReport(_CRT_WARN, file, line, NULL, "warning: %s\n", message.data());
     }
-    
+    inline void ShowWarning(std::string_view message) {
+        ShowWarning(ASSERT_FILE_LINE, message);
+    }
+    inline void RaiseAssert(char const* file, int line, std::string_view message) {
+        _CrtDbgReport(_CRT_ERROR, file, line, NULL, "error: %s\n", message.data());
+    }
     inline void RaiseAssert(std::string_view message) {
-        _CrtDbgReport(_CRT_ERROR, __FILE__, __LINE__, NULL, "error: %s\n", message.data());
+        RaiseAssert(ASSERT_FILE_LINE, message);
     }
 #else
-    inline void ShowWarning(std::string_view message) {
-        message; // C4100 'identifier' : unreferenced formal parameter
-    }
-    inline void RaiseAssert(std::string_view message) {
-        message; // C4100 'identifier' : unreferenced formal parameter
-    }
+    inline void ShowWarning(char const*, int = 0, std::string_view = "") {}
+    inline void RaiseAssert(char const*, int = 0, std::string_view = "") {}
 #endif // _DEBUG
-    
     inline void ExceptionThrow(std::string_view message) {
         throw std::exception(message.data());
     }
