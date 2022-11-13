89aed61c08affa61910fec482a3eee494777a7f3
shirokuma1101 <71858151+shirokuma1101@users.noreply.github.com>
2022-11-13 21:55:18 +0900

    update: Vector2Dクラスを追加、放物運動の計算クラスを追加、コードの整形、コメントの追加

--- Src/Math/MinimumMath.h
+++ Src/Math/MinimumMath.h
@@ -8,7 +8,56 @@
 #pragma warning(push)
 #pragma warning(disable:4201) // nonstandard extension used : nameless struct/union
 
+/**************************************************
+*
+* A class that implements minimal (inefficient)
+* math functions
+*
+**************************************************/
 namespace minimum_math {
+
+    /**************************************************
+    * 2D Vector
+    **************************************************/
+    struct Vector2 {
+        Vector2()                             noexcept : x(0), y(0) {}
+        Vector2(float s)                      noexcept : x(s), y(s) {}
+        Vector2(float x, float y)             noexcept : x(x), y(y) {}
+
+        // Unary
+        Vector2 operator-()             const noexcept { return Vector2(-x, -y); }
+        // Assignment
+        Vector2& operator=(const Vector2& v)  noexcept { x = v.x;  y =  v.y; return *this; }
+        // Compound assignment
+        Vector2& operator+=(const Vector2& v) noexcept { x += v.x; y += v.y; return *this; }
+        Vector2& operator-=(const Vector2& v) noexcept { x -= v.x; y -= v.y; return *this; }
+        Vector2& operator*=(float s)          noexcept { x *= s;   y *= s;   return *this; }
+        Vector2& operator*=(const Vector2& v) noexcept { x *= v.x; y *= v.y; return *this; }
+        Vector2& operator/=(float s)          noexcept { x /= s;   y /= s;   return *this; }
+        Vector2& operator/=(const Vector2& v) noexcept { x /= v.x; y /= v.y; return *this; }
+
+        float Dot(const Vector2& v)     const noexcept { return x * v.x + y * v.y; }
+
+        float Length()                  const noexcept { return std::sqrt(Dot(*this)); }
+        float LengthSquared()           const noexcept { return Dot(*this); }
+    
+        union {
+            struct { float x, y; };
+            float v2[2];
+        };
+    };
+    // Arithmetic
+    Vector2 operator+(const Vector2& v1, const Vector2& v2) noexcept { return Vector2(v1) += v2; }
+    Vector2 operator-(const Vector2& v1, const Vector2& v2) noexcept { return Vector2(v1) -= v2; }
+    Vector2 operator*(const Vector2& v,  float s)           noexcept { return Vector2(v)  *= s;  }
+    Vector2 operator*(const Vector2& v1, const Vector2& v2) noexcept { return Vector2(v1) *= v2; }
+    Vector2 operator/(const Vector2& v,  float s)           noexcept { return Vector2(v)  /= s;  }
+    Vector2 operator/(const Vector2& v1, const Vector2& v2) noexcept { return Vector2(v1) /= v2; }
+    
+
+    /**************************************************
+    * 3D Vector
+    **************************************************/
     struct Vector3 {
         Vector3()                             noexcept : x(0), y(0), z(0) {}
         Vector3(float s)                      noexcept : x(s), y(s), z(s) {}
@@ -32,14 +81,24 @@ namespace minimum_math {
         float   Length()                const noexcept { return std::sqrt(Dot(*this)); }
         float   LengthSquared()         const noexcept { return Dot(*this); }
         
-        float x, y, z;
+        union {
+            struct { float x, y, z; };
+            float v3[3];
+        };
     };
     // Arithmetic
     Vector3 operator+(const Vector3& v1, const Vector3& v2) noexcept { return Vector3(v1) += v2; }
     Vector3 operator-(const Vector3& v1, const Vector3& v2) noexcept { return Vector3(v1) -= v2; }
     Vector3 operator*(const Vector3& v,  float s)           noexcept { return Vector3(v)  *= s;  }
     Vector3 operator*(const Vector3& v1, const Vector3& v2) noexcept { return Vector3(v1) *= v2; }
+    Vector3 operator/(const Vector3& v,  float s)           noexcept { return Vector3(v)  /= s;  }
+    Vector3 operator/(const Vector3& v1, const Vector3& v2) noexcept { return Vector3(v1) /= v2; }
 
+    /**************************************************
+    * 4x4 matrix with left-handed coordinate system
+    * default (right-handed coordinate system if
+    * MINIMUMMATH_MATRIX_RIGHT_HANDED is defined)
+    **************************************************/
     struct Matrix4x4 {
         Matrix4x4() noexcept
             : m00(1), m01(0), m02(0), m03(0)
@@ -56,26 +115,26 @@ namespace minimum_math {
             , m30(m30), m31(m31), m32(m32), m33(m33) {}
 
         // Assignment
-        Matrix4x4& operator=(const Matrix4x4& m4x4) noexcept {
-            m00 = m4x4.m00; m01 = m4x4.m01; m02 = m4x4.m02; m03 = m4x4.m03;
-            m10 = m4x4.m10; m11 = m4x4.m11; m12 = m4x4.m12; m13 = m4x4.m13;
-            m20 = m4x4.m20; m21 = m4x4.m21; m22 = m4x4.m22; m23 = m4x4.m23;
-            m30 = m4x4.m30; m31 = m4x4.m31; m32 = m4x4.m32; m33 = m4x4.m33;
+        Matrix4x4& operator=(const Matrix4x4& m) noexcept {
+            m00 = m.m00; m01 = m.m01; m02 = m.m02; m03 = m.m03;
+            m10 = m.m10; m11 = m.m11; m12 = m.m12; m13 = m.m13;
+            m20 = m.m20; m21 = m.m21; m22 = m.m22; m23 = m.m23;
+            m30 = m.m30; m31 = m.m31; m32 = m.m32; m33 = m.m33;
             return *this;
         }
         // Compound assignment
-        Matrix4x4& operator+=(const Matrix4x4& m4x4) noexcept {
-            m00 += m4x4.m00; m01 += m4x4.m01; m02 += m4x4.m02; m03 += m4x4.m03;
-            m10 += m4x4.m10; m11 += m4x4.m11; m12 += m4x4.m12; m13 += m4x4.m13;
-            m20 += m4x4.m20; m21 += m4x4.m21; m22 += m4x4.m22; m23 += m4x4.m23;
-            m30 += m4x4.m30; m31 += m4x4.m31; m32 += m4x4.m32; m33 += m4x4.m33;
+        Matrix4x4& operator+=(const Matrix4x4& m) noexcept {
+            m00 += m.m00; m01 += m.m01; m02 += m.m02; m03 += m.m03;
+            m10 += m.m10; m11 += m.m11; m12 += m.m12; m13 += m.m13;
+            m20 += m.m20; m21 += m.m21; m22 += m.m22; m23 += m.m23;
+            m30 += m.m30; m31 += m.m31; m32 += m.m32; m33 += m.m33;
             return *this;
         }
-        Matrix4x4& operator-=(const Matrix4x4& m4x4) noexcept {
-            m00 -= m4x4.m00; m01 -= m4x4.m01; m02 -= m4x4.m02; m03 -= m4x4.m03;
-            m10 -= m4x4.m10; m11 -= m4x4.m11; m12 -= m4x4.m12; m13 -= m4x4.m13;
-            m20 -= m4x4.m20; m21 -= m4x4.m21; m22 -= m4x4.m22; m23 -= m4x4.m23;
-            m30 -= m4x4.m30; m31 -= m4x4.m31; m32 -= m4x4.m32; m33 -= m4x4.m33;
+        Matrix4x4& operator-=(const Matrix4x4& m) noexcept {
+            m00 -= m.m00; m01 -= m.m01; m02 -= m.m02; m03 -= m.m03;
+            m10 -= m.m10; m11 -= m.m11; m12 -= m.m12; m13 -= m.m13;
+            m20 -= m.m20; m21 -= m.m21; m22 -= m.m22; m23 -= m.m23;
+            m30 -= m.m30; m31 -= m.m31; m32 -= m.m32; m33 -= m.m33;
             return *this;
         }
         Matrix4x4& operator*=(float s) noexcept {
@@ -86,19 +145,19 @@ namespace minimum_math {
             return *this;
         }
 
-#ifdef RIGHT_HANDED
-        Vector3 Up()                 const noexcept { return Vector3( m20,  m21,  m22); }
-        Vector3 Down()               const noexcept { return Vector3(-m20, -m21, -m22); }
-        Vector3 Right()              const noexcept { return Vector3( m10,  m11,  m12); }
-        Vector3 Left()               const noexcept { return Vector3(-m10, -m11, -m12); }
-        Vector3 Forward()            const noexcept { return Vector3( m00,  m01,  m02); }
-        Vector3 Backward()           const noexcept { return Vector3(-m00, -m01, -m02); }
-        void    Up(const Vector3& v)       noexcept { m20 =  v.x; m21 =  v.y; m22 =  v.z; }
-        void    Down(const Vector3& v)     noexcept { m20 = -v.x; m21 = -v.y; m22 = -v.z; }
-        void    Right(const Vector3& v)    noexcept { m10 =  v.x; m11 =  v.y; m12 =  v.z; }
-        void    Left(const Vector3& v)     noexcept { m10 = -v.x; m11 = -v.y; m12 = -v.z; }
-        void    Forward(const Vector3& v)  noexcept { m00 =  v.x; m01 =  v.y; m02 =  v.z; }
-        void    Backward(const Vector3& v) noexcept { m00 = -v.x; m01 = -v.y; m02 = -v.z; }
+#ifdef MINIMUMMATH_MATRIX_RIGHT_HANDED
+        Vector3 Up()                    const noexcept { return Vector3( m20,  m21,  m22); }
+        Vector3 Down()                  const noexcept { return Vector3(-m20, -m21, -m22); }
+        Vector3 Right()                 const noexcept { return Vector3( m10,  m11,  m12); }
+        Vector3 Left()                  const noexcept { return Vector3(-m10, -m11, -m12); }
+        Vector3 Forward()               const noexcept { return Vector3( m00,  m01,  m02); }
+        Vector3 Backward()              const noexcept { return Vector3(-m00, -m01, -m02); }
+        void    Up(const Vector3& v)          noexcept { m20 =  v.x; m21 =  v.y; m22 =  v.z; }
+        void    Down(const Vector3& v)        noexcept { m20 = -v.x; m21 = -v.y; m22 = -v.z; }
+        void    Right(const Vector3& v)       noexcept { m10 =  v.x; m11 =  v.y; m12 =  v.z; }
+        void    Left(const Vector3& v)        noexcept { m10 = -v.x; m11 = -v.y; m12 = -v.z; }
+        void    Forward(const Vector3& v)     noexcept { m00 =  v.x; m01 =  v.y; m02 =  v.z; }
+        void    Backward(const Vector3& v)    noexcept { m00 = -v.x; m01 = -v.y; m02 = -v.z; }
 #else
         Vector3 Up()                    const noexcept { return Vector3( m10,  m11,  m12); }
         Vector3 Down()                  const noexcept { return Vector3(-m10, -m11, -m12); }
@@ -113,7 +172,7 @@ namespace minimum_math {
         void    Forward(const Vector3& v)     noexcept { m20 = -v.x; m21 = -v.y; m22 = -v.z; }
         void    Backward(const Vector3& v)    noexcept { m20 =  v.x; m21 =  v.y; m22 =  v.z; }
 #endif
-        Vector3 Translation()           const noexcept { return Vector3( m30,  m31,  m32); }
+        Vector3 Translation()           const noexcept { return Vector3( m30,  m31,  m32);   }
         void    Translation(const Vector3& v) noexcept { m30 =  v.x; m31 =  v.y; m32 =  v.z; }
 
         union {
@@ -123,7 +182,7 @@ namespace minimum_math {
                 float m20, m21, m22, m23;
                 float m30, m31, m32, m33;
             };
-            float m[4][4];
+            float m4x4[4][4];
         };
     };
     // Arithmetic
@@ -135,7 +194,7 @@ namespace minimum_math {
         for (int row = 0; row < 4; ++row) {
             for (int col = 0; col < 4; ++col) {
                 for (int inner = 0; inner < 4; ++inner) {
-                    m.m[row][col] += m1.m[row][inner] * m2.m[inner][col];
+                    m.m4x4[row][col] += m1.m4x4[row][inner] * m2.m4x4[inner][col];
                 }
             }
         }
--- Src/Math/ProjectileMotion.h
+++ Src/Math/ProjectileMotion.h
@@ -14,6 +14,34 @@
 #pragma warning(disable:4201) // nonstandard extension used : nameless struct/union
 
 struct ProjectileMotion {
+    ProjectileMotion() noexcept
+        : velocity(0)
+        , vectorx(0)
+        , vectory(0)
+        , theta(0)
+        , time(0)
+        , length(0)
+        , height(0)
+        , gravity(constant::fG)
+    {}
+    ProjectileMotion(float velocity, float vectorx, float vectory, float theta, float time, float length, float height, float gravity = constant::fG) noexcept
+        : velocity(velocity)
+        , vectorx(vectorx)
+        , vectory(vectory)
+        , theta(theta)
+        , time(time)
+        , length(length)
+        , height(height)
+        , gravity(gravity)
+    {}
+    
+    std::tuple<float, float> DisplacementPosition(float _time) const noexcept {
+        return { vx * _time, vy * _time - convert::ToHalf(g * convert::ToSquare(_time)) };
+    }
+    std::tuple<float, float> DisplacementVector(float _time) const noexcept {
+        return { vx, vy - (g * _time) };
+    }
+    
     union {
         struct {
             float velocity;
@@ -36,50 +64,6 @@ struct ProjectileMotion {
             float g;
         };
     };
-
-    ProjectileMotion() noexcept
-        : velocity(0.f)
-        , vectorx(0.f)
-        , vectory(0.f)
-        , theta(0.f)
-        , time(0.f)
-        , length(0.f)
-        , height(0.f)
-        , gravity(constant::fG)
-    {}
-    constexpr ProjectileMotion(float velocity, float vectorx, float vectory, float theta, float time, float length, float height, float gravity = constant::fG) noexcept
-        : velocity(velocity)
-        , vectorx(vectorx)
-        , vectory(vectory)
-        , theta(theta)
-        , time(time)
-        , length(length)
-        , height(height)
-        , gravity(gravity)
-    {}
-
-    std::tuple<float, float> DisplacementPosition(float _time) const noexcept {
-        return { vx * _time, vy * _time - (1.f / 2.f) * g * convert::ToSquare(_time) };
-    }
-
-    std::tuple<float, float> DisplacementVector(float _time) const noexcept {
-        return { vx, vy - (g * _time) };
-    }
-};
-
-struct ProjectileMotionFromTimeLength : public ProjectileMotion {
-    ProjectileMotionFromTimeLength() noexcept
-        : ProjectileMotion(0.f, 0.f, 0.f,  0.f, 0.f, 0.f, 0.f)
-    {}
-    ProjectileMotionFromTimeLength(float time, float length, float gravity = constant::fG) noexcept
-        : ProjectileMotion(0.f, 0.f, 0.f, 0.f, time, length, 0.f, gravity)
-    {
-        v0    = std::sqrt(convert::ToSquare(l / t) + convert::ToSquare(g * t / 2.f));
-        theta = std::atan((g * convert::ToSquare(t)) / (2.f * l));
-        vx    = v0 * std::cos(theta);
-        vy    = v0 * std::sin(theta);
-        h     = g * convert::ToSquare(t) / 8.0f; //(1.f / 2.f) * g * convert::ToSquare(t / 2.f);
-    }
 };
 
 struct ProjectileMotionFromVelocityTheta : public ProjectileMotion {
@@ -97,130 +81,119 @@ struct ProjectileMotionFromVelocityTheta : public ProjectileMotion {
     }
 };
 
-struct ProjectileMotionFromVelocityLength {
+struct ProjectileMotionFromHeightLength : public ProjectileMotion {
+    ProjectileMotionFromHeightLength() noexcept
+        : ProjectileMotion(0.f, 0.f, 0.f, 0.f, 0.f, 0.f, 0.f)
+    {}
+    ProjectileMotionFromHeightLength(float height, float length, float gravity = constant::fG) noexcept
+        : ProjectileMotion(0.f, 0.f, 0.f, 0.f, 0.f, length, height, gravity)
+    {
+        theta = std::atan(4.f * h / l);
+        v0    = std::sqrt(2.f * g * h) / std::sin(theta);
+        vx    = v0 * std::cos(theta);
+        vy    = v0 * std::sin(theta);
+        t     = 2.f * std::sqrt(2.f * h / g);
+    }
+};
+
+//struct ProjectileMotionFromVelocityTime : public ProjectileMotion {
+//    ProjectileMotionFromVelocityTime() noexcept
+//        : ProjectileMotion(0.f, 0.f, 0.f, 0.f, 0.f, 0.f, 0.f)
+//    {}
+//    ProjectileMotionFromVelocityTime(float velocity, float time, float gravity = constant::fG) noexcept
+//        : ProjectileMotion(velocity, 0.f, 0.f, 0.f, time, 0.f, 0.f, gravity)
+//    {
+//        l     = std::sqrt(convert::ToSquare(v0) - convert::ToSquare(convert::ToHalf(g * t) * t)); //TODO: expr bug
+//        h     = g * convert::ToSquare(t) / 8.f;
+//        theta = std::atan(4.f * h / l);
+//        vx    = v0 * std::cos(theta);
+//        vy    = v0 * std::sin(theta);
+//    }
+//};
+
+struct ProjectileMotionFromTimeLength : public ProjectileMotion {
+    ProjectileMotionFromTimeLength() noexcept
+        : ProjectileMotion(0.f, 0.f, 0.f, 0.f, 0.f, 0.f, 0.f)
+    {}
+    ProjectileMotionFromTimeLength(float time, float length, float gravity = constant::fG) noexcept
+        : ProjectileMotion(0.f, 0.f, 0.f, 0.f, time, length, 0.f, gravity)
+    {
+        v0    = std::sqrt(convert::ToSquare(l / t) + convert::ToSquare(g * t / 2.f));
+        vx    = v0 * std::cos(theta);
+        vy    = v0 * std::sin(theta);
+        theta = std::atan((g * convert::ToSquare(t)) / (2.f * l));
+        h     = g * convert::ToSquare(t) / 8.0f;
+    }
+};
+
+struct ProjectileMotionFromThetaLength : public ProjectileMotion {
+    ProjectileMotionFromThetaLength() noexcept
+        : ProjectileMotion(0.f, 0.f, 0.f, 0.f, 0.f, 0.f, 0.f)
+    {}
+    ProjectileMotionFromThetaLength(float theta, float length, float gravity = constant::fG) noexcept
+        : ProjectileMotion(0.f, 0.f, 0.f, theta, 0.f, length, 0.f, gravity)
+    {
+        v0 = std::sqrt(g * l / (2.f * std::sin(theta) * std::cos(theta)));
+        vx = v0 * std::cos(theta);
+        vy = v0 * std::sin(theta);
+        t  = l / vx;
+        h  = g * convert::ToSquare(t) / 8.f;
+    }
+};
+
+struct ProjectileMotionFromThetaHeight : public ProjectileMotion {
+    ProjectileMotionFromThetaHeight() noexcept
+        : ProjectileMotion(0.f, 0.f, 0.f, 0.f, 0.f, 0.f, 0.f)
+    {}
+    ProjectileMotionFromThetaHeight(float theta, float height, float gravity = constant::fG) noexcept
+        : ProjectileMotion(0.f, 0.f, 0.f, theta, 0.f, 0.f, height, gravity)
+    {
+        v0 = std::sqrt(2.f * g * h) / std::sin(theta);
+        vx = v0 * std::cos(theta);
+        vy = v0 * std::sin(theta);
+        t  = (2.f * std::sqrt(2.f * g * h)) / g;
+        l  = t * vx;
+    }
+};
+
+struct ProjectileMotionFromVelocityLength : public ProjectileMotion {
+    ProjectileMotionFromVelocityLength() noexcept
+        : ProjectileMotion(0.f, 0.f, 0.f, 0.f, 0.f, 0.f, 0.f)
+    {}
+    ProjectileMotionFromVelocityLength(float velocity, float length, float gravity = constant::fG) noexcept
+        : ProjectileMotion(velocity, 0.f, 0.f, 0.f, 0.f, length, 0.f, gravity)
+    {
+        //TODO: 高さを考慮する場合は有効フラグを切り替える
+        if (l == 0.f) {
+            parabolicMotions[0].isEnable = true;
+            parabolicMotions[1].isEnable = true;
+            parabolicMotions[0].projectileMotionFromVelocityTheta = ProjectileMotionFromVelocityTheta(v0, 0.f, gravity);
+            parabolicMotions[1].projectileMotionFromVelocityTheta = ProjectileMotionFromVelocityTheta(-v0, 0.f, gravity);
+        }
+        else {
+            float a    = (-g * convert::ToSquare(l)) / (2.f * convert::ToSquare(v0));
+            float b    = length / a;
+            float c    = 1.f; // 高さを考慮する場合: (a - pos_vec) / a;
+            float root = std::sqrt(-c + convert::ToSquare(b) / 4.f);
+            float d    = convert::ToSquare(b) / 4.f - c;
+            if (d < 0.f) {
+                parabolicMotions[0].isEnable = false;
+                parabolicMotions[1].isEnable = false;
+            }
+            else {
+                parabolicMotions[0].isEnable = true;
+                parabolicMotions[1].isEnable = true;
+                parabolicMotions[0].projectileMotionFromVelocityTheta = ProjectileMotionFromVelocityTheta(v0, std::atan((-b / 2.f) + root), gravity);
+                parabolicMotions[1].projectileMotionFromVelocityTheta = ProjectileMotionFromVelocityTheta(v0, std::atan((-b / 2.f) - root), gravity);
+            }
+        }
+    }
+
     struct ParabolicMotion {
         bool                              isEnable = false;
         ProjectileMotionFromVelocityTheta projectileMotionFromVelocityTheta;
     };
     std::array<ParabolicMotion, 2> parabolicMotions;
-
-    ProjectileMotionFromVelocityLength() = default;
-    ProjectileMotionFromVelocityLength(float velocity, float length, float gravity = constant::fG) noexcept {
-        for (int i = 0; i < 2; ++i) {
-            parabolicMotions[i].isEnable                          = false;
-            parabolicMotions[i].projectileMotionFromVelocityTheta = { velocity, 0.f, gravity };
-        }
-
-        if (length == 0.f) {
-            //note: 高さを考慮する場合は有効フラグを切り替える
-            parabolicMotions[0].projectileMotionFromVelocityTheta.theta = std::atan2(velocity, 0.f);
-            parabolicMotions[1].projectileMotionFromVelocityTheta.theta = std::atan2(-velocity, 0.f);
-            parabolicMotions[0].isEnable = true;
-            parabolicMotions[1].isEnable = true;
-        }
-        else {
-            float a    = (-gravity * convert::ToSquare(length)) / (2.f * convert::ToSquare(velocity));
-            float b    = length / a;
-            float c    = 1.f; // a / a; // 高さを考慮する場合: (a - pos_vec) / a;
-            float root = std::sqrt(-c + convert::ToSquare(b) / 4.f);
-            float t    = convert::ToSquare(b) / 4.f - c;
-
-            if (t < 0.f) {
-                parabolicMotions[0].isEnable = false;
-                parabolicMotions[1].isEnable = false;
-            }
-            else {
-                parabolicMotions[0].projectileMotionFromVelocityTheta.theta = std::atan((-b / 2.f) + root);
-                parabolicMotions[1].projectileMotionFromVelocityTheta.theta = std::atan((-b / 2.f) - root);
-                parabolicMotions[0].isEnable = true;
-                parabolicMotions[1].isEnable = true;
-            }
-        }
-    }
-
-    ProjectileMotionFromVelocityTheta NarrowAngleDisplacement() const {
-        return NarrowAngleDisplacement([&](int i, float) {
-            return parabolicMotions[i].projectileMotionFromVelocityTheta;
-        },
-        0
-        );
-    }
-    std::tuple<float, float> NarrowAngleDisplacementPosition(float time) const {
-        return NarrowAngleDisplacement([&](int i, float time) {
-            return parabolicMotions[i].projectileMotionFromVelocityTheta.DisplacementPosition(time);
-        },
-        time
-        );
-    }
-    std::tuple<float, float> NarrowAngleDisplacementVector(float time) const {
-        return NarrowAngleDisplacement([&](int i, float time) {
-            return parabolicMotions[i].projectileMotionFromVelocityTheta.DisplacementVector(time);
-        },
-        time
-        );
-    }
-
-    ProjectileMotionFromVelocityTheta WideAngleDisplacement() const {
-        return WideAngleDisplacement([&](int i, float) {
-            return parabolicMotions[i].projectileMotionFromVelocityTheta;
-        },
-        0
-        );
-    }
-    std::tuple<float, float> WideAngleDisplacementPosition(float time) const {
-        return WideAngleDisplacement([&](int i, float time) {
-            return parabolicMotions[i].projectileMotionFromVelocityTheta.DisplacementPosition(time);
-        },
-        time
-        );
-    }
-    std::tuple<float, float> WideAngleDisplacementVector(float time) const {
-        return WideAngleDisplacement([&](int i, float time) {
-            return parabolicMotions[i].projectileMotionFromVelocityTheta.DisplacementVector(time);
-        },
-        time
-        );
-    }
-    
-    bool IsEnable() const noexcept {
-        return parabolicMotions[0].isEnable || parabolicMotions[1].isEnable;
-    }
-
-private:
-
-    template<class Func>
-    auto NarrowAngleDisplacement(Func func, float time) const {
-        if (parabolicMotions[0].isEnable && parabolicMotions[1].isEnable) {
-            if (parabolicMotions[0].projectileMotionFromVelocityTheta.theta < parabolicMotions[1].projectileMotionFromVelocityTheta.theta) {
-                return func(0, time);
-            }
-            return func(1, time);
-        }
-        else if (parabolicMotions[0].isEnable) {
-            return func(0, time);
-        }
-        else if (parabolicMotions[1].isEnable) {
-            return func(1, time);
-        }
-        return decltype(func(0, 0))();
-    }
-
-    template<class Func>
-    auto WideAngleDisplacement(Func func, float time) const {
-        if (parabolicMotions[0].isEnable && parabolicMotions[1].isEnable) {
-            if (parabolicMotions[0].projectileMotionFromVelocityTheta.theta > parabolicMotions[1].projectileMotionFromVelocityTheta.theta) {
-                return func(0, time);
-            }
-            return func(1, time);
-        }
-        else if (parabolicMotions[0].isEnable) {
-            return func(0, time);
-        }
-        else if (parabolicMotions[1].isEnable) {
-            return func(1, time);
-        }
-        return decltype(func(0, 0))();
-    }
 };
 
 #pragma warning(pop)
