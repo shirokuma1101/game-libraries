e7b82f5b9e690a9df53de770a1d8163336113a0d
shirokuma1101 <71858151+shirokuma1101@users.noreply.github.com>
2022-12-18 19:52:42 +0900

    add: jsonを保持し必要な時に更新できるクラスの追加

---
+++ Inc/ExternalDependencies/Asset/Json/JsonHolder.h
@@ -0,0 +1,40 @@
+﻿#pragma once
+
+#include "nlohmann/json.hpp"
+
+struct IJsonData {
+    virtual void FromJson(const nlohmann::json& json) = 0;
+};
+
+template <class T>
+class JsonHolder
+{
+public:
+
+    JsonHolder(nlohmann::json* json)
+        : m_pJson(json)
+    {
+        static_assert(std::is_base_of_v<IJsonData, T>, "T must be derived from IJsonData");
+        Load();
+    }
+
+    T& Get() {
+        return m_data;
+    }
+    const T& Get() const {
+        return m_data;
+    }
+
+    bool Load() {
+        if (!m_pJson) return false;
+        m_data.FromJson(*m_pJson);
+        return true;
+    }
+
+private:
+
+    const nlohmann::json* m_pJson = nullptr;
+    
+    T                    m_data;
+
+};
