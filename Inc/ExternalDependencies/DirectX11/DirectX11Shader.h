2b1fe46c4a9380bd2d0643370e3cc80d23050e18
shirokuma1101 <71858151+shirokuma1101@users.noreply.github.com>
2023-05-10 22:56:17 +0900

    add: Shaderの基底クラスの一部追加

---
+++ Inc/ExternalDependencies/DirectX11/DirectX11Shader.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+
+#ifndef GAME_LIBRARIES_EXTERNALDEPENDENCIES_DIRECTX11_DIRECTX11SHADER_H_
+#define GAME_LIBRARIES_EXTERNALDEPENDENCIES_DIRECTX11_DIRECTX11SHADER_H_
+
+#include "Utility/Assert.h"
+#include "Utility/Memory.h"
+#include "DirectX11Helper.h"
+
+template<class T>
+class DirectX11Shader
+{
+public:
+
+    DirectX11Shader(ID3D11Device* dev)
+        : m_pDev(dev)
+    {}
+    virtual ~DirectX11Shader() {
+        Release();
+    }
+
+protected:
+
+    ID3D11Device* m_pDev = nullptr;
+
+};
+
+#endif
