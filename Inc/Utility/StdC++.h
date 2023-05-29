7191a8830db475af11046fe81e4ecc7decb87600
shirokuma1101 <71858151+shirokuma1101@users.noreply.github.com>
2023-05-30 00:00:51 +0900

    refactor: IncludeするC互換ヘッダーの変更

--- Inc/Utility/StdC++.h
+++ Inc/Utility/StdC++.h
@@ -14,7 +14,7 @@
 
 /**
  * @brief C++ Standard Library headers.
- * @note "https://en.cppreference.com/w/cpp/header"
+ * @note "https://en.cppreference.com/w/cpp/header" reference. (2023-05-21)
  */
 
 // Concepts library
@@ -168,8 +168,8 @@
 // Localization library
 #include <clocale>
 #include <locale>
-#if (__cplusplus >= MACRO_CPP11) && (__cplusplus < MACRO_CPP17)
-#include <codecvt> // deprecated:C++17
+#if __cplusplus >= MACRO_CPP11 && __cplusplus < MACRO_CPP17
+#include <codecvt> // deprecated in C++17
 #endif
 
 // Input / output library
@@ -184,7 +184,7 @@
 #include <sstream>
 #include <streambuf>
 #if __cplusplus < MACRO_CPP98
-#include <strstream> // deprecated:C++98
+#include <strstream> // deprecated in C++98
 #endif
 #if __cplusplus >= MACRO_CPP20
 #include <syncstream>
@@ -226,41 +226,40 @@
 #include <stop_token>
 #endif
 
-
 // C compatibility headers
-#include <cassert>
-#include <cctype>
-#include <cerrno>
-#include <cfloat>
-#include <climits>
-#include <clocale>
-#include <cmath>
-#include <csetjmp>
-#include <csignal>
-#include <cstdarg>
-#include <cstddef>
-#include <cstdio>
-#include <cstdlib>
-#include <cstring>
-#include <ctime>
-#include <cwchar>
-#include <cwctype>
+#include <assert.h> // Behaves same as <cassert>
+#include <ctype.h>  // Behaves as if each name from <cctype> is placed in global namespace
+#include <errno.h>  // Behaves same as <cerrno>
+#include <float.h>  // Behaves same as <cfloat>
+#include <limits.h> // Behaves same as <climits>
+#include <locale.h> // Behaves as if each name from <clocale> is placed in global namespace
+#include <math.h>   // Behaves as if each name from <cmath> is placed in global namespace, except for names of mathematical special functions
+#include <setjmp.h> // Behaves as if each name from <csetjmp> is placed in global namespace
+#include <signal.h> // Behaves as if each name from <csignal> is placed in global namespace
+#include <stdarg.h> // Behaves as if each name from <cstdarg> is placed in global namespace
+#include <stddef.h> // Behaves as if each name from <cstddef> is placed in global namespace, except for names of std::byte and related functions
+#include <stdio.h>  // Behaves as if each name from <cstdio> is placed in global namespace
+#include <stdlib.h> // Behaves as if each name from <cstdlib> is placed in global namespace
+#include <string.h> // Behaves as if each name from <cstring> is placed in global namespace
+#include <time.h>   // Behaves as if each name from <ctime> is placed in global namespace
+#include <wchar.h>  // Behaves as if each name from <cwchar> is placed in global namespace
+#include <wctype.h> // Behaves as if each name from <cwctype> is placed in global namespace
 #if __cplusplus >= MACRO_CPP11
-#include <cfenv>
-#include <cinttypes>
-#include <cstdint>
-#include <cuchar>
-#endif
-
-// Empty C headers
-#if (__cplusplus >= MACRO_CPP11) && (__cplusplus < MACRO_CPP17)
-#include <ccomplex> // deprecated:C++17/removed:C++20
-#include <complex.h>
-#include <ctgmath>  // deprecated:C++17/removed:C++20
-#include <tgmath.h>
+#include <fenv.h>     // Behaves as if each name from <cfenv> is placed in global namespace
+#include <inttypes.h> // Behaves as if each name from <cinttypes> is placed in global namespace
+#include <stdint.h>   // Behaves as if each name from <cstdint> is placed in global namespace
+#include <uchar.h>    // Behaves as if each name from <cuchar> is placed in global namespace
 #endif
 
 // Special C compatibility headers
+#if __cplusplus >= MACRO_CPP11
+//#include <complex.h>
+//#include <tgmath.h>
+#endif
+#if __cplusplus >= MACRO_CPP11 && __cplusplus < MACRO_CPP17
+#include <ccomplex> // deprecated in C++17 / removed in C++20
+#include <ctgmath> // deprecated in C++17 / removed in C++20
+#endif
 #if __cplusplus >= MACRO_CPP23
 #include <stdatomic.h>
 #endif
@@ -268,13 +267,13 @@
 // Meaningless C headers
 #include <iso646.h>
 #if __cplusplus >= MACRO_CPP11
-//#include <stdalign.h> // 環境による?
-#include <stdbool.h>
-#if __cplusplus < MACRO_CPP17
-#include <cstdalign> // deprecated:C++17/removed:C++20
-#include <cstdbool>  // deprecated:C++17/removed:C++20
+//#include <stdalign.h>
+//#include <stdbool.h>
 #endif
+#if __cplusplus >= MACRO_CPP11 && __cplusplus < MACRO_CPP17
+#include <cstdalign> // deprecated in C++17 / removed in C++20
+#include <cstdbool> // deprecated in C++17 / removed in C++20
 #endif
 #if __cplusplus < MACRO_CPP20
-#include <ciso646> // removed:C++20
+#include <ciso646> // removed in C++20
 #endif
